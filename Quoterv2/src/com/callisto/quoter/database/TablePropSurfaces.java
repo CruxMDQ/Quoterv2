00101fc12392e9f8a179ecad18e94822e5038c41
Emiliano <cribraider@hotmail.com>
2013-03-22 00:34:37 -0300

    Added table definition for property surfaces.

---
+++ Quoterv2/src/com/callisto/quoter/database/TablePropSurfaces.java
@@ -0,0 +1,38 @@
+package com.callisto.quoter.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class TablePropSurfaces
+{
+	public static final String 
+		TABLE_PROPERTY_SURFACES = "Propiedades_superficies",
+		COLUMN_SURFACE_BUILT = "Construida",
+		COLUMN_SURFACE_COVERED = "Cubierta",
+		COLUMN_SURFACE_SEMICOVERED = "Semicubierta",
+		COLUMN_SURFACE_NOT_BUILT = "Descubierta",
+		DATABASE_CREATE = "create table " + TABLE_PROPERTY_SURFACES + "("
+				+ TableProperties.COLUMN_ID_PROPERTY + " integer primary key, "
+				+ COLUMN_SURFACE_BUILT + " text,"
+				+ COLUMN_SURFACE_COVERED + " text,"
+				+ COLUMN_SURFACE_SEMICOVERED + " text,"
+				+ COLUMN_SURFACE_NOT_BUILT + " text,"
+				+ "FOREIGN KEY(" + TableProperties.COLUMN_ID_PROPERTY + ") REFERENCES " + TableProperties.TABLE_PROPERTIES + "(" + TableProperties.COLUMN_ID_PROPERTY + ")"
+				+ ");";
+	
+	public static void onCreate(SQLiteDatabase db)
+	{
+		db.execSQL(DATABASE_CREATE);
+	}
+	
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		Log.w(TableCities.class.getName(), "Upgrading database from version " + oldVersion 
+				+ " to " + newVersion
+				+ ", which will destroy all old data");
+		
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROPERTY_SURFACES);
+		
+		onCreate(db);
+	}
+}
